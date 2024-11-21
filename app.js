0abedd8ec86ad5a327e4d0f721f243df62c848f7
sxcueto <sxcueto@gmail.com>
2024-11-20 19:22:35 -0500

    push style/js update

--- app.js
+++ app.js
@@ -1,12 +1,69 @@
-// fetch('https://api.sampleapis.com/coffee/hot')
-//want to display title, ingredients, and image
 
-   const getHot = async () => {
-    const url = 'https://api.sampleapis.com/coffee/hot';
-    const reponse = await fetch (url);
-    return await response.json();
-   };
+// const title = document.getElementById('title');
+// const ingredients = document.getElementById('ingredients');
+// const image = document.getElementById('image');
+// let hotCoffee = []
 
-   const render = (hot) => {
-    return hot.map(({}))
+
+fetch('https://api.sampleapis.com/coffee/hot')
+  .then ((response) => {
+   if (response.ok) {
+      return response.json();
+   } else{
+      throw new Errror ("Can't fetch hot drinks.");
    }
+  })
+
+  .then(data => {
+   console.log(data);
+   displayCoffee(data)
+  })
+.catch(error => console.error('Error fetching coffee menu:', error));
+
+function displayCoffee(data) {
+   const coffee = data.coffee[0]
+   const coffeeDiv = document.getElementById('coffee')
+
+
+const coffeeName = coffee.strHot;
+const heading = document.createElement("h3");
+heading.innerHTML = coffeeName;
+coffeeDiv.appendChild(heading);
+
+//coffee image
+const coffeeImg = document.createElement('img');
+coffeeImg.src = coffee.strDrinkThumb;
+coffeeDiv.appendChild(coffeeImg);
+document.body.style.backgroundImage = "url ('" + coffee.strDrinkThumb + "')";
+
+//coffee ingredients
+const coffeeIngredients = document.createElement("ul");
+coffee.Div.appendChild(coffeeIngredients);
+const getIngredients = Object.keys(coffee)
+.filter(function (ingredient) {
+   if (coffee[ingredient] !=null) {
+      ingredient[ingredient] = coffee[ingredient];
+   }
+      return ingredient;}, {}
+
+);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
